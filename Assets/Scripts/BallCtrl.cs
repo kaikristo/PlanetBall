8c0ff01cad1d61586676df67024e060e294229a2
KaiKristo <KaiKristo@gmail.com>
2019-10-25 11:55:04 +1000

    init commit

---
+++ Assets/Scripts/BallCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test.Ball
+{
+
+    public class BallCtrl : MonoBehaviour
+    {
+        private Rigidbody2D ballRigidBody;
+
+        void Start()
+        {
+            ballRigidBody = GetComponent<Rigidbody2D>();
+        }
+
+    }
+}
